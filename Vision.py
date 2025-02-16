bcdd43e64946d38c7c64ccfff7bf97260d83de93
MamakSquad <mamakranger@gmail.com>
2025-02-16 08:40:27 +0800

    Update Vision.py

--- Vision.py
+++ Vision.py
@@ -3,28 +3,12 @@ import json
 from google.cloud import vision
 from google.oauth2 import service_account
 from flask import Flask, jsonify, request
-from firebase_admin import auth, firestore, credentials, initialize_app
-# Initialize Firebase
-cred = credentials.Certificate("./dr-kokua-firebase-adminsdk-fbsvc-b63dcc4b93.json")
-initialize_app(cred)
-
-# Firestore client
-db = firestore.client()
 
 app = Flask(__name__)
 
 # Initialize Google Vision Client
 class VisionAPI:
-    def __init__(self):
-        # Load credentials from file
-        credential_path = "./vision_key.json"
-        with open(credential_path, "r") as cred_file:
-            credentials_json = json.load(cred_file)
-
-        # Authenticate using the loaded credentials
-        credentials = service_account.Credentials.from_service_account_info(credentials_json)
-        self.client = vision.ImageAnnotatorClient(credentials=credentials)
-
+   
     def detect_text(self, image_path: str) -> str:
         """Detects and extracts text from an image using Google Cloud Vision API."""
         with open(image_path, "rb") as image_file:
@@ -72,37 +56,18 @@ class VisionAPI:
                 extracted_info["description"] = line.strip()
 
         return extracted_info
+pass
 
 
-# Create VisionAPI instance
-vision_api = VisionAPI()
-
-@app.route('/detect', methods=['POST'])
-def detect_text_endpoint():
-    # Get the image file from the request
-    image = request.files['image']  # Assuming the image is sent in a multipart form-data
-
-    # Save the image temporarily to process
-    image_path = 'temp_image.jpg'
-    image.save(image_path)
-    print("Image received!")
-
-    try:
-        # Call the detect_text function
-        text = vision_api.detect_text(image_path)
-        extracted_info = vision_api.extract_info(text)
-        print("Text Succesfully Detected")
-
-        # Store the extracted information in Firestore
-        db.collection("users").document(user_uid).collection("medication_records").document()
-        print("Medicine Info Successfully Stored")
-
-        # Return the extracted information as JSON
-        return jsonify(extracted_info)
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
 
 # Run the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+"""""
+    print("\nExtracted Information:")
+    print(f"Patient Name: {data["patient_name"]}")
+    print(f"Medicine Name: {data["medicine_name"]}")
+    print(f"Amount to Consume: {data["amount"]} biji")
+    print(f"Frequency: {data["frequency"]}")
+    print(f"Description: {data[{"description"}]}")
+"""""
